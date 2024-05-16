5e0466776c21b415671fcad919a1548d0023a300
Aaquib Equbal <armanaaquibequbal@gamil.com>
2024-05-16 15:13:58 +0530

    Add generics examples

---
+++ generics/asserts_test.go
@@ -0,0 +1,20 @@
+package generics
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+
+	t.Run("asserting on bools", func(t *testing.T) {
+		AssertTrue(t, true)
+		AssertFalse(t, false)
+	})
+}
